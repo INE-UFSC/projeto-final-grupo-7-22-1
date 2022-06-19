2e9995afdde4631e99b967178028cc2c24f09458
medalha01 <hiperboss2009@gmail.com>
2022-06-19 16:52:58 -0300

    Função colisão e Hit

--- prototipo/Collision.py
+++ prototipo/Collision.py
@@ -4,5 +4,16 @@ import pygame
 from Character import Character
 from Region import Region
 
-class Collision():
-    
+
+class Collision:
+    def __init__(self):
+        self.x_modificador = -15
+
+    def Hit(self, Player, Objects):
+        Hit_Boolean = pygame.sprite.spritecollide(Player, Objects, False)
+        return Hit_Boolean
+
+    def Update_Hit(self, Player, Objects):
+        HitVar = self.Hit(Player, Objects)
+        if HitVar:
+            Player.stop()
